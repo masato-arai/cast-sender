37ccb1ea0b14691dbed8993369336154c7d21fc0
masato-arai <masato@ntslive.co.uk>
2024-09-13 22:13:17 +0100

    Initial Commit

---
+++ CastSender/CastSenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CastSenderApp.swift
+//  CastSender
+//
+//  Created by Masato Arai on 13/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct CastSenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
