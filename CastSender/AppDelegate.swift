63549a7d131b79330f40759a45097f2420a9d337
masato-arai <masato@ntslive.co.uk>
2024-09-23 16:06:24 +0100

    Implement basic app delegation class

--- CastSender/AppDelegate.swift
+++ CastSender/AppDelegate.swift
@@ -8,45 +8,62 @@
 import UIKit
 import GoogleCast
 
-class AppDelegate: UIResponder, UIApplicationDelegate, GCKLoggerDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    fileprivate var enableSDKLogging = true
+
     let kReceiverAppID = kGCKDefaultMediaReceiverApplicationID
     let kDebugLoggingEnabled = true
 
     var window: UIWindow?
 
-    func applicationDidFinishLaunching(_ application: UIApplication) {
-        print("Your code here")
-
+    func application(
+        _: UIApplication,
+        didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
         let criteria = GCKDiscoveryCriteria(applicationID: kReceiverAppID)
         let options = GCKCastOptions(discoveryCriteria: criteria)
+        options.physicalVolumeButtonsWillControlDeviceVolume = true
         GCKCastContext.setSharedInstanceWith(options)
 
-        // Enable logger.
+        window?.clipsToBounds = true
+        setupCastLogging()
+        
+//        // Enable logger.
+//        GCKLogger.sharedInstance().delegate = self
+//        print("Hello")
+        return true
+    }
+
+    func setupCastLogging() {
+        let logFilter = GCKLoggerFilter()
+        let classesToLog = [
+            "GCKDeviceScanner",
+            "GCKDeviceProvider",
+            "GCKDiscoveryManager",
+            "GCKCastChannel",
+            "GCKMediaControlChannel",
+            "GCKUICastButton",
+            "GCKUIMediaController",
+            "NSMutableDictionary"
+        ]
+        logFilter.setLoggingLevel(.verbose, forClasses: classesToLog)
+        GCKLogger.sharedInstance().filter = logFilter
         GCKLogger.sharedInstance().delegate = self
     }
+}
 
-    // MARK: - GCKLoggerDelegate
+// MARK: - GCKLoggerDelegate
 
-    func logMessage(_ message: String,
-                    at level: GCKLoggerLevel,
-                    fromFunction function: String,
-                    location: String) {
-        if (kDebugLoggingEnabled) {
-            print(function + " - " + message)
+extension AppDelegate: GCKLoggerDelegate {
+    func logMessage(
+        _ message: String,
+        at _: GCKLoggerLevel,
+        fromFunction function: String,
+        location: String
+    ) {
+        if enableSDKLogging {
+            // Send SDK's log messages directly to the console.
+            print("\(location): \(function) - \(message)")
         }
     }
-
-//    func application(_: UIApplication,
-//                     didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-//
-//        let options = GCKCastOptions(discoveryCriteria: GCKDiscoveryCriteria(applicationID: kReceiverAppID))
-//        options.physicalVolumeButtonsWillControlDeviceVolume = true
-//        GCKCastContext.setSharedInstanceWith(options)
-//
-//        window?.clipsToBounds = true
-//        setupCastLogging()
-//
-//        print("Your code here")
-//        return
-//    }
 }
