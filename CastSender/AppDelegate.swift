bf2afc0b3734a2f45ad10bd57d114455df832c78
masato-arai <masato@ntslive.co.uk>
2024-10-14 14:41:37 +0100

    Custom UI test

--- CastSender/AppDelegate.swift
+++ CastSender/AppDelegate.swift
@@ -25,6 +25,38 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         options.physicalVolumeButtonsWillControlDeviceVolume = true
         GCKCastContext.setSharedInstanceWith(options)
 
+        // Customise UI
+        let castStyle = GCKUIStyle.sharedInstance()
+        let backgroundColor: UIColor = UIColor.black
+        let foregroundColor: UIColor = UIColor.white
+
+        castStyle.castViews.backgroundColor = backgroundColor
+        castStyle.castViews.iconTintColor = foregroundColor
+        castStyle.castViews.bodyTextColor = foregroundColor
+        castStyle.castViews.headingTextColor = foregroundColor
+        castStyle.castViews.buttonTextColor = UIColor.green
+
+        // Navigation
+        GCKUIStyle.sharedInstance().castViews.deviceControl.connectionController.navigation.backgroundColor = backgroundColor
+
+        GCKUIStyle.sharedInstance().castViews.deviceControl.connectionController.navigation.headingTextColor = foregroundColor
+//        GCKUIStyle.sharedInstance().castViews.headingTextFont = UIFont(.universBold, size: 18)
+
+        GCKUIStyle.sharedInstance().castViews.deviceControl.connectionController.navigation.buttonTextColor = foregroundColor
+
+        // Content - Device selection
+        castStyle.castViews.deviceControl.deviceChooser.backgroundColor = backgroundColor
+        castStyle.castViews.deviceControl.deviceChooser.headingTextColor = foregroundColor
+        castStyle.castViews.deviceControl.deviceChooser.iconTintColor = foregroundColor
+
+        // Connection Controller
+        GCKUIStyle.sharedInstance().castViews.deviceControl.connectionController.iconTintColor = UIColor.green
+//        GCKUIStyle.sharedInstance().castViews.deviceControl.connectionController.playImage = UIImage(named: "quote")!
+        GCKUIStyle.sharedInstance().castViews.deviceControl.connectionController.captionTextColor = UIColor.green
+
+        castStyle.apply()
+
+
         window?.clipsToBounds = true
         setupCastLogging()
         
